7e382bc01fff985c178e78e577176178821c0d14
nassan <sand.ss376@gmail.com>
2025-05-02 18:37:41 +0900

    IDを管理するヘッダーをつくってみた

---
+++ src/components/common/ids.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <library/wobc.h>
+
+namespace component {
+    namespace id {
+        // コンポーネントID
+        static const uint8_t PRESSURE = 0x25;
+        static const uint8_t IMU = 0x20;
+        static const uint8_t Drive = 0x29;
+        static const uint8_t Separate = 0x28;
+        static const uint8_t GPS = 0x2A;
+
+        // テレメトリID
+
+    }
+}
